139359191d4a194bbc20ef89bfbe334a5d3d506a
redachl <chliehreda@gmail.com>
2022-06-20 18:47:07 +0200

    Use a stack instead of a queue for possible paths
    Time limit exceeded on test 4 again, even with a filter before adding to
    an ongoing path to the stack.

--- main.py
+++ main.py
@@ -6,30 +6,29 @@ def compute_required_energy(intersection_count: int, shortcuts: List[int]):
     energy_required_to_ith_interesection = [0] * intersection_count
     for i in range(2, intersection_count + 1):
         position = 1
-        position_and_energy_queue = [(1, 0)]
-        possible_energies = [abs(i - position)]
-        while position_and_energy_queue:
-            position, energy = position_and_energy_queue.pop(0)
+        
+        # Use a depth first search (follow a path using shortcuts or going forward or backward and discard it when the minimum energy is reached)
+        position_and_energy_stack = [(1, 0)]
+        minimum_energy = abs(i - position)
+        while position_and_energy_stack and minimum_energy > 1:
+            position, energy = position_and_energy_stack.pop()
 
             # stop trying to move to the target intersection when it is reached
-            if position == i:
-                possible_energies.append(energy)
-                continue
-
-            # stop trying to reach the target intersection when the energy used is greater than or equal to the maximum energy |1 - i|
-            if energy >= possible_energies[0]:
+            if position == i and energy < minimum_energy:
+                minimum_energy = energy
                 continue
 
             next_with_shortcut = shortcuts[position - 1]
-            if next_with_shortcut != position:
-                position_and_energy_queue.append(
+            # for all paths, stop trying to reach the target intersection when the energy used is greater than or equal to the maximum energy |1 - i|
+            if next_with_shortcut != position and energy + 1 < minimum_energy and not (minimum_energy - (energy + 1) == 1 and next_with_shortcut < len(shortcuts) and shortcuts[next_with_shortcut] != i and abs(i - next_with_shortcut) != 1):
+                position_and_energy_stack.append(
                     (next_with_shortcut, energy + 1))
-            if position + 1 < intersection_count:
-                position_and_energy_queue.append((position + 1, energy + 1))
-            if position - 1 > 0:
-                position_and_energy_queue.append((position - 1, energy + 1))
+            if position + 1 < intersection_count and energy + 1 < minimum_energy and not (minimum_energy - (energy + 1) == 1 and next_with_shortcut < len(shortcuts) and shortcuts[next_with_shortcut] != i and abs(i - next_with_shortcut) != 1):
+                position_and_energy_stack.append((position + 1, energy + 1))
+            if position - 1 > 0 and energy + 1 < minimum_energy and not (minimum_energy - (energy + 1) == 1 and next_with_shortcut < len(shortcuts) and shortcuts[next_with_shortcut] != i and abs(i - next_with_shortcut) != 1):
+                position_and_energy_stack.append((position - 1, energy + 1))
 
-        energy_required_to_ith_interesection[i - 1] = min(possible_energies)
+        energy_required_to_ith_interesection[i - 1] = minimum_energy
 
     return energy_required_to_ith_interesection
 
